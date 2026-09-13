61ace6cc8e45709c219493d6011e3bd7cea48a79
Syn <141505017+thecoolersyn@users.noreply.github.com>
2026-09-13 17:36:29 +0700

    Create gamelist.lua

---
+++ gamelist.lua
@@ -0,0 +1,57 @@
+local gamelist = {
+    {
+        GameName = "Sell Lemons",
+        GameConfig = "SL",
+        UniverseId = 7395930870,
+        CreatorName = "BloxByte Games",
+    },
+
+    {
+        GameName = "Murder Mystery 2",
+        GameConfig = "MM2",
+        UniverseId = 66654135,
+        CreatorName = "Nikilis",
+    },
+
+    {
+        GameName = "Pet Simulator 99",
+        GameConfig = "PS99",
+        UniverseId = 3317771874,
+        CreatorName = "BIG Games Pets",
+    },
+
+    {
+        GameName = "Fisch",
+        GameConfig = "Fisch",
+        UniverseId = 5750914919,
+        CreatorName = "Fisching",
+    },
+
+    {
+        GameName = "Driving Empire",
+        GameConfig = "DE",
+        UniverseId = 1202096104,
+        CreatorName = "Driving Empire by Voldex",
+    },
+
+    {
+        GameName = "Tower Defense Simulator",
+        GameConfig = "TDS",
+        UniverseId = 1176784616,
+        CreatorName = "Paradoxum Games",
+    },
+
+    {
+        GameName = "Jailbreak",
+        GameConfig = "JB",
+        UniverseId = 245662005,
+        CreatorName = "Badimo",
+    },
+
+    {
+        GameName = "Sell Ores",
+        GameConfig = "SO",
+        UniverseId = 10336278580,
+        CreatorName = "The Ore Drillers",
+    },
+}
